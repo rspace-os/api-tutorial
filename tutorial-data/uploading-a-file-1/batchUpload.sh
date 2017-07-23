8090f5f89d8089eccd7d0ed005f7422336728e95
Richard Adams <richard@researchspace.com>
2017-07-23 11:41:01 +0100

    docs

--- tutorial-data/uploading-a-file-1/batchUpload.sh
+++ tutorial-data/uploading-a-file-1/batchUpload.sh
@@ -1,3 +1,4 @@
+## Uploads a list of files supplied on the command line.
 for file in $*; do
 curl -v  -X POST -H "accept: application/json" -H "apiKey: <APIKEY>" -H "content-type: multipart/form-data"\
   -F "file=@${file}"  "<RSPACE_URL>/api/v1/files"
