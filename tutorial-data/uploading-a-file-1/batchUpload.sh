c0dcfc402a959039a962be65c3f06f91d5feec0a
Richard Adams <richard@researchspace.com>
2017-07-23 11:40:00 +0100

    formatting

--- tutorial-data/uploading-a-file-1/batchUpload.sh
+++ tutorial-data/uploading-a-file-1/batchUpload.sh
@@ -1,3 +1,4 @@
 for file in $*; do
-curl -v  -X POST -H "accept: application/json" -H "apiKey: <APIKEY>" -H "content-type: multipart/form-data" -F "file=@${file}"  "<RSPACE_URL>/api/v1/files"
+curl -v  -X POST -H "accept: application/json" -H "apiKey: <APIKEY>" -H "content-type: multipart/form-data"\
+  -F "file=@${file}"  "<RSPACE_URL>/api/v1/files"
 done;
